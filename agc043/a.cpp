860c549d85a0c6cd8e08cac199ae33ff17f8370e
uribo <ur1b0.dev@gmail.com>
2020-03-21 22:44:47 +0900

    壁伝いで行けるか確認するのを実装できた

--- agc043/a.cpp
+++ agc043/a.cpp
@@ -31,48 +31,40 @@ int main(){
 
     // 壁伝いに動けるマスを探す
     int x = 0;
-    int y = 1;
+    int y = 0;
     int xold = -1;
     int yold = -1;
 
-    for( i = 0; i < 3; i++ ){
-        cout << "x:" << x << "\n";
-        cout << "y:" << y << "\n";
+    while( 1 ){
+        cout << "now:" << x << "," << y  << "  old:" << xold << "," << yold << "\n";
         // 上確認
-        if( ( x > 0 ) && ( x != xold ) ){
-            cout << "up\n";
-            if( li[ x + 1 ][y] == 46 ){
-                cout << "up ok\n";
-                x++;
-            }
+        if( ( x > 0 ) && ( ( x - 1 ) != xold ) && ( li[ x - 1 ][y] == 46 ) ){
+            cout << "up ok\n";
+            xold = x;
+            x--;
         }
         // 右確認
-        else if( ( y < b ) && ( y != yold ) ){
-            cout << "right\n";
-            if( li[x][ y + 1 ] == 46 ){
-                cout << "right ok\n";
-                y++;
-            }
-        }
-        // 左確認
-        else if( ( y > 0 ) && ( y != yold ) ){
-            cout << "left\n";
-            if( li[x][ y - 1 ] == 46 ){
-                cout << "left ok\n";
-                y--;
-            }
+        else if( ( y < b ) && ( ( y + 1 ) != yold ) && ( li[x][ y + 1 ] == 46 ) ){
+            cout << "right ok\n";
+            yold = y;
+            y++;
         }
         // 下確認
-        else if( ( x < a ) && ( x != xold ) ){
-            cout << "down\n";
-            if( li[ x - 1 ][y] == 46 ){
-                cout << "down ok\n";
-                x--;
-            }
+        else if( ( x < a ) && ( ( x + 1 ) != xold ) && ( li[ x + 1 ][y] == 46 ) ){
+            cout << "down ok\n";
+            xold = x;
+            x++;
+        }
+        // 左確認
+        else if( ( y > 0 ) && ( ( y - 1 ) != yold ) && ( li[x][ y - 1 ] == 46 ) ){
+            cout << "left ok\n";
+            yold = y;
+            y--;
         }
         // どこもいけない
         else{
             cout << "nothing\n";
+            break;
         }
 
         // もとの位置に戻ってきたか、ゴールしたか確認
@@ -80,10 +72,23 @@ int main(){
             cout << "nothing else\n";
             break;
         }
-        else if( ( x == a ) && ( y == b ) ){
+        else if( ( x == a - 1  ) && ( y == b - 1 ) ){
             cout << "goal\n";
             break;
         }
-    }
+    } 
 
 }
+
+/*
+4 4
+..##
+#...
+###.
+###.
+
+3 4
+..##
+#...
+###.
+*/
